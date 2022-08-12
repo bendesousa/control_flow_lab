77a639116e373067853719d02c6824979d9cdef7
bendesousa <bdsousa069@gmail.com>
2022-08-12 08:48:01 +0100

    finished outift selector

--- src/main/java/WhatShouldIWear.java
+++ src/main/java/WhatShouldIWear.java
@@ -4,7 +4,6 @@ public class WhatShouldIWear {
 
     public static void main(String[] args) {
         System.out.println("Daily Outfit Selector");
-//        Determine possible outputs
 //        Prompt the user to input the weather from a list
         System.out.println("PLease pick one of the following weather conditions: Sun, Rain, Snow,");
 //        Collect user input
@@ -13,7 +12,22 @@ public class WhatShouldIWear {
 //        Prompt the user to input average temperature
         System.out.println("What is today's forecasted average temperature?");
 //        Collect user input
+        Scanner numerator = new Scanner(System.in);
+        int temperature = numerator.nextInt();
 //        Select an output based on user input
+        if (weather.equals("Rain") && temperature <= 14){
+            System.out.println("Waterproof jacket and trousers");
+        } else if (weather.equals("Rain") && temperature >= 15){
+            System.out.println("Waterproof jacket and shorts");
+        } else if (weather.equals("Sun") && temperature <= 14){
+            System.out.println("Hoodie and trousers");
+        } else if (weather.equals("Sun") && temperature >= 15){
+            System.out.println("T-shirt and shorts");
+        } else if (weather.equals("Snow") && temperature <= 14){
+            System.out.println("Winter coat and trousers");
+        } else if (weather.equals("Snow") && temperature >= 15){
+            System.out.println("Hoodie and trousers");
+        }
 
     }
 
